486c1d12b6e872851f7d2c1a50b2149dd6555788
Dmitry Vlasov <kerbalspacema@gmail.com>
2021-03-02 15:12:08 +0300

    Fixes

--- dndserver.py
+++ dndserver.py
@@ -2,17 +2,20 @@ import os
 
 from flask import Flask, json
 
-app = Flask(__name__)
-
 
 def get_json():
     jsons = {}
     for file in os.listdir("./files"):
         if ".json" in file:
             print(file, "Found")
-            jsons[file.split(".")[0]] = json.load(open(file, encoding="utf-8"))
+            jsons[file.split(".")[0]] = json.load(open("./files/" + file, encoding="utf-8"))
     return jsons
 
+
+app = Flask(__name__)
+print("App created")
+files = get_json()
+print("JSON created")
 
 @app.route('/files/<file>', methods=['GET'])
 def get_files(file):
@@ -22,6 +25,3 @@ def get_files(file):
 @app.route('/', methods=['GET'])
 def get_welcome():
     return 'Welcome to this unfinished stuff!'
-
-
-files = get_json()
--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,6 @@ import os
 from dndserver import app
 
 if __name__ == "__main__":
+    print("Starting app")
     app.run(port=os.environ.get("PORT", 5000))
+    print("App started")
