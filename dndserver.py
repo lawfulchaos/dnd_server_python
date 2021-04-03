3d1fddda804338d1324a8e4d2167956aca72ec22
Dmitry Vlasov <kerbalspacema@gmail.com>
2021-04-03 10:01:47 +0300

    db fixes

--- dndserver.py
+++ dndserver.py
@@ -24,48 +24,51 @@ print("App created")
 db.init_app(app)
 db = initialize_flask_sqlathanor(db)
 
-def search_items(item_list, pattern):
-    return item_list.query.filter(*[getattr(Item, col_name).like("%" + pattern[col_name] + "%") for col_name in pattern]).all()
-
-
-#
-#
-# def add_to_db(itemlist, itemtype="Spell"):
-#     translation_table = {"Описание": "description", "Название": "name",
-#                          "Источник": "source", "Качество": "quality", "Изображения": "images",
-#                          "Харизма": "charisma", "Чувства": "senses", "Способности": "abilities",
-#                          "Сопротивление урону": "resistance", "Интеллект": "intelligence",
-#                          "Сила": "strength", "Легендарные действия": "legendary_acts",
-#                          "Уязвимость к урону": "vulnerability", "Класс доспеха": "armor_class",
-#                          "Навыки": "skills", "Мудрость": "wisdom",
-#                          "Иммунитет к статусу": "status_immunities",
-#                          "Логово": "lair", "Ловкость": "agility", "Языки": "languages",
-#                          "Скорость": "speed",
-#                          "Тип доспеха": "armor_type", "Действия": "actions",
-#                          "Действия логова": "lair_acts",
-#                          "Спасброски": "saves", "Эффекты логова": "lair_effects",
-#                          "Телосложение": "constitution",
-#                          "Опасность": "danger", "Хиты": "hits", "Кубы хитов": "hit_dice",
-#                          "Иммунитет к урону": "dmg_immunities", "Реакции": "reactions",
-#                          'Время накладывания': "cast_time", 'Дистанция': "distance",
-#                          'Длительность': "time_active", 'Классы': "classes", 'Компоненты':
-#                              "components", 'Школа': "school"
-#                          }
-#     for item in itemlist:
-#         for key in item.copy():
-#             if key in translation_table:
-#                 item[translation_table[key]] = item.pop(key)
-#         if itemtype != "Spell":
-#             to_add = Beast(**{k: v for k, v in item.items() if
-#                               k not in {'Магический предмет добавил', "Монстра добавил",
-#                                         "Заклинание добавил"}})
-#         else:
-#             to_add = Spell(**{k: v for k, v in item.items() if
-#                               k not in {'Магический предмет добавил', "Монстра добавил",
-#                                         "Заклинание добавил"}})
-#         db.session.add(to_add)
-#     db.session.commit()
 from models import Beast, Spell, Item
+
+
+def search_items(item_list, pattern):
+    return item_list.query.filter(
+        *[getattr(item_list, col_name).like("%" + pattern[col_name] + "%") for col_name in
+          pattern]).all()
+
+
+def add_to_db(itemlist, itemtype="Spell"):
+    translation_table = {"Описание": "description", "Название": "name",
+                         "Источник": "source", "Качество": "quality", "Изображения": "images",
+                         "Харизма": "charisma", "Чувства": "senses", "Способности": "abilities",
+                         "Сопротивление урону": "resistance", "Интеллект": "intelligence",
+                         "Сила": "strength", "Легендарные действия": "legendary_acts",
+                         "Уязвимость к урону": "vulnerability", "Класс доспеха": "armor_class",
+                         "Навыки": "skills", "Мудрость": "wisdom",
+                         "Иммунитет к статусу": "status_immunities",
+                         "Логово": "lair", "Ловкость": "agility", "Языки": "languages",
+                         "Скорость": "speed",
+                         "Тип доспеха": "armor_type", "Действия": "actions",
+                         "Действия логова": "lair_acts",
+                         "Спасброски": "saves", "Эффекты логова": "lair_effects",
+                         "Телосложение": "constitution",
+                         "Опасность": "danger", "Хиты": "hits", "Кубы хитов": "hit_dice",
+                         "Иммунитет к урону": "dmg_immunities", "Реакции": "reactions",
+                         'Время накладывания': "cast_time", 'Дистанция': "distance",
+                         'Длительность': "time_active", 'Классы': "classes", 'Компоненты':
+                             "components", 'Школа': "school"
+                         }
+    for item in itemlist:
+        for key in item.copy():
+            if key in translation_table:
+                item[translation_table[key]] = item.pop(key)
+        if itemtype != "Spell":
+            to_add = Beast(**{k: v for k, v in item.items() if
+                              k not in {'Магический предмет добавил', "Монстра добавил",
+                                        "Заклинание добавил"}})
+        else:
+            to_add = Spell(**{k: v for k, v in item.items() if
+                              k not in {'Магический предмет добавил', "Монстра добавил",
+                                        "Заклинание добавил"}})
+        db.session.add(to_add)
+    db.session.commit()
+
 
 entry_names = {"spell": Spell, "beast": Beast, "item": Item}
 
